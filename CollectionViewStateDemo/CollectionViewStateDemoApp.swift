aa2c6fcc7cfe69ca6dbc7da4312b0d0b06046a53
Matthew Lorentz <matt@lorentz.is>
2024-03-04 15:49:02 -0500

    Initial Commit

---
+++ CollectionViewStateDemo/CollectionViewStateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewStateDemoApp.swift
+//  CollectionViewStateDemo
+//
+//  Created by Matthew Lorentz on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CollectionViewStateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
